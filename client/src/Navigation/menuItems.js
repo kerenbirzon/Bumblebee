11db3c74fc4d99b19a7d5816d95f15687fdf8756
Tamir Amar <tamir.amar@zerto.com>
2022-03-06 14:23:42 +0200

    adding navigation folder

---
+++ client/src/Navigation/menuItems.js
@@ -0,0 +1,101 @@
+import { costomImage, govilImage, dhlImage } from "../images/projectImages";
+
+export const managerMenuItems = [
+  {
+    title: "Home Page",
+    path: "/homepage",
+    isDisabled: false,
+  },
+  {
+    title: "About Us",
+    path: "/about-us",
+    isDisabled: false,
+  },
+  // {
+  //   title: "My Profile",
+  //   path: "/my-profile",
+  //   isDisabled: false,
+  // },
+  {
+    title: "Mange Users",
+    path: "/our-users",
+    isDisabled: false,
+  },
+  {
+    title: "Add New Car",
+    path: "/create-car",
+    isDisabled: false,
+  },
+  // {
+  //   title: "My Favorite",
+  //   path: "/my-favorite",
+  //   isDisabled: false,
+  // },
+  // {
+  //   title: "Gov IL Form",
+  //   image: govilImage,
+  //   path: "/govil",
+  //   isDisabled: false,
+  // },
+  {
+    title: "DHL Label form",
+    image: dhlImage,
+    path: "/dhl-label",
+    isDisabled: false,
+  },
+  {
+    title: "Customs Broker",
+    image: costomImage,
+    path: "/customs-broker",
+    isDisabled: false,
+  },
+];
+
+export const clientMenuItems = [
+  {
+    title: "Home Page",
+    path: "/homepage",
+    isDisabled: false,
+    role: "1",
+  },
+  {
+    title: "About Us",
+    path: "/about-us",
+    isDisabled: false,
+    role: "1",
+  },
+  // {
+  //   title: "My Profile",
+  //   path: "/my-profile",
+  //   isDisabled: false,
+  //   role: "1",
+  // },
+  // {
+  //   title: "My Favorite",
+  //   path: "/my-favorite",
+  //   isDisabled: false,
+  //   role: "1",
+  // },
+  // {
+  //   title: "Gov IL Form",
+  //   image: govilImage,
+  //   path: "/govil",
+  //   isDisabled: false,
+  //   role: "1",
+  // },
+  {
+    title: "DHL Label form",
+    image: dhlImage,
+    path: "/dhl-label",
+    isDisabled: false,
+    role: "1",
+  },
+  {
+    title: "Customs Broker",
+    image: costomImage,
+    path: "/customs-broker",
+    isDisabled: false,
+    role: "1",
+  },
+];
+    
