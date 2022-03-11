479faca12f1cc4626de0c12e9497d453d7a3bbb4
linoybekker <linoylbekker@gmail.com>
2022-03-11 21:17:38 +0200

    Changeing the sending way

--- client/src/components/CarComponents/CarImageGallery.jsx
+++ client/src/components/CarComponents/CarImageGallery.jsx
@@ -1,11 +1,10 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import DealerCard from "./DealerCard";
 import { useAuth } from "../../contexts/AuthContext";
 
 export default function CarImageGallery({ id, car }) {
-  
   // const url = "https://firebasestorage.googleapis.com/v0/b/bumblebee-d5c23.appspot.com/o/files%2F";
   const url = "https://bumblebee-pro.s3.eu-west-1.amazonaws.com/";
   const [dealer, setDealer] = useState(car.dealer);
@@ -15,9 +14,8 @@ export default function CarImageGallery({ id, car }) {
       .then((response) => response.json())
       .then((data) => {
         setDealer(data);
-        console.log(data);
       });
-  }, [dealer]);
+  }, []);
 
   return (
     <>
@@ -32,27 +30,26 @@ export default function CarImageGallery({ id, car }) {
           sx={{ width: 200, height: 500 }}
           cols={1}
         >
-          {car.images
-            ? car.images.map((image) => {
-            return (
-              <ImageListItem key={image}>
-                <img
-                  src={image}
-                />
-              </ImageListItem>
-            );
+          {car.images && car.images.length > 0 ? (
+            car.images.map((image) => {
+              return (
+                <ImageListItem key={image}>
+                  <img src={url + image} />
+                </ImageListItem>
+              );
             })
-            : null}
+          ) : (
+            <ImageListItem key={1}>
+              <img src="/noimages.png" />
+            </ImageListItem>
+          )}
         </ImageList>
         <div style={{ flexBasis: "50%", height: 450 }}>
           <img
             src={
-              car.images
-                ? car.images[0]
-                : //  url +
-                  //   car.images[0].substring(0, car.images[0].indexOf(",")) +
-                  //   "?alt=media"
-                  null
+              car.images && car.images.length > 0
+                ? url + car.images[0]
+                : "/image_not_available.png"
             }
             width={"100%"}
             height={450}
@@ -60,38 +57,10 @@ export default function CarImageGallery({ id, car }) {
           />
         </div>
         <div style={{ flexBasis: "33%", marginLeft: "1%" }}>
-          <DealerCard
-            firstName={dealer ? dealer.firstName : null}
-            lastName={dealer ? dealer.lastName : null}
-            email={dealer ? dealer.email : null}
-            phoneNumber={"+972-522520484"}
-            image={"/profileImage.png"}
-          />
+          <DealerCard dealer={dealer ? dealer : null} />
         </div>
       </div>
     </>
   );
 }
-// const itemData = [
-//   {
-//     img: "/mini.jpeg",
-//   },
-//   {
-//     img: "/mini.jpeg",
-//   },
-//   {
-//     img: "/mini.jpeg",
-//   },
-//   {
-//     img: "/mini.jpeg",
-//   },
-//   {
-//     img: "/mini.jpeg",
-//   },
-//   {
-//     img: "/mini.jpeg",
-//   },
-//   {
-//     img: "/mini.jpeg",
-//   },
-// ];
+
